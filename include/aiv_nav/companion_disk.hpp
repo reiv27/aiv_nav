6f2a581657dcebb63c1b9877218e477f2e0226fc
Roman Eidelman <eidelmanrv@gmail.com>
2025-10-06 22:23:13 +0300

    UPD: Update disk class

--- include/aiv_nav/companion_disk.hpp
+++ include/aiv_nav/companion_disk.hpp
@@ -5,8 +5,18 @@
 class CompanionDisk
 {
 public:
-  CompanionDisk(double R_min, double R, double ro_0);
+  CompanionDisk(double R, int resolution);
+
+  void update_disk_data(std::vector<double>& lidar_ranges, std::vector<double>& robot_pose);
+
+  double get_disk_position() const;
+  std::vector<double> get_rays_length() const;
+
 private:
-  double R;
-  double resolution_;
+  double R_;
+  contst double resolution_;
+
+  std::vector<double> disk_pose_ = {0.0, 0.0};
+  std::vector<double> rays_length_(resolution_, 0.0);
+  double min_ray_length_ = 0.0;
 };
